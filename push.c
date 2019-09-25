fb578f4e5b3164f11bd92f4e5d20f345af5182d3
Brent Muha <800@holbertonschool.com>
2019-09-25 16:39:41 +0000

    Add push

---
+++ push.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * op_push - Push an element to the stack
+ * @stack: The stack
+ * @line_number: The line number
+ */
+void op_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+
+	if (argument == -7777)
+	{
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n",
+			line_number);
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = argument;
+	new->next = *stack;
+	new->prev = NULL;
+	if (*stack)
+		(*stack)->prev = new;
+	*stack = new;
+}
